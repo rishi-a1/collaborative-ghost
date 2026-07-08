3f439b0ba14f40b7d383288162727c6032badb85
rishi-a1 <hrishikesh.akella@gmail.com>
2026-07-09 01:32:05 +0400

    Made db and linked to main

--- backend/main.py
+++ backend/main.py
@@ -7,3 +7,21 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+app = FastAPI ( )
+models. Base.metadata. create_all (bind=engine)
+class ChoiceBase (BaseModel):
+    choice_text: str 
+    is_correct: bool
+class QuestionBase (BaseModel):
+    question_text: str
+    choices: List [ChoiceBase]
+def get_db() :
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+    db.close()
