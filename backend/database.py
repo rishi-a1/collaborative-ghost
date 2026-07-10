68eb59847adb8dadb41916dbd7b86219ae5f4050
rishi-a1 <hrishikesh.akella@gmail.com>
2026-07-10 14:15:53 +0400

    developing endpoints

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
+
+#Getting the database url from the .env
 current_dir = os.path.dirname(os.path.abspath(__file__))
 env_path = os.path.join(os.path.dirname(current_dir), ".env")
 load_dotenv(dotenv_path=env_path)
-print(os.getenv("DB_URL"))
+db_url = os.getenv("DB_URL")
+
+engine = create_engine(db_url)
+Sessionlocal = sessionmaker(autocommit=False, autoflush = False, bind=engine)
+Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Annotated
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-app = FastAPI ( )
-models. Base.metadata. create_all (bind=engine)
-class ChoiceBase (BaseModel):
-    choice_text: str 
-    is_correct: bool
-class QuestionBase (BaseModel):
-    question_text: str
-    choices: List [ChoiceBase]
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
 def get_db() :
     db = SessionLocal()
     try:
         yield db
     finally:
-    db.close()
+        db.close()
+
+@app.post("/")
+async def root():
+    return {"message": "Hello World"}
