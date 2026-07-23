89205678c4e4cee758321e9722bbbb2bf84ccd19
rishi-a1 <hrishikesh.akella@gmail.com>
2026-07-23 15:18:26 +0400

    Made basic home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
-  return <div>Home Page</div>
+  return (
+    <div className="home-container">
+      <h1>Collaborative Ghost Writer</h1>
+      <p>Craft stories together in real-time.</p>
+      
+      <div className="button-group">
+        <Link to="/join">
+          <button className="btn btn-secondary">Join Story</button>
+        </Link>
+        
+        <Link to="/create">
+          <button className="btn btn-primary">Create Story</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
